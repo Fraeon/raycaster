d18cc0e00f774dd499742fc141a66dd39e8b166b
fraeon <me@fraeon.info>
2015-11-20 03:13:28 +0200

    Pikkujuttuja

---
+++ player.js
@@ -0,0 +1,120 @@
+//player.js
+//contains player movement
+var PI = Math.PI
+var FULLCIRCLE = PI * 2;
+
+//Control object
+//
+//Contains the properties required for control
+function Control() {
+
+    //Poll for key press
+    this.keypress = function(v, e) {
+        var state = this.codes[e.keyCode];
+        if (typeof state === 'undefined') {
+            return;
+        }
+        this.states[state] = v;
+        e.preventDefault && e.preventDefault();
+        e.stopPropagation && e.stopPropagation();
+    }
+
+    //Poll for touch input
+    this.touch = function(e) {
+        var t = e.touches[0];
+        this.touchend(e);
+        if (t.pageY < window.innerHeight * 0.5) {
+            this.keypress(true, {
+                keyCode: 38
+            });
+        } else if (t.pageX < window.innerWidth * 0.5) {
+            this.keypress(true, {
+                keyCode: 37
+            });
+        } else if (t.pageY > window.innerWidth * 0.5) {
+            this.keypress(true, {
+                keyCode: 39
+            });
+        }
+    }
+
+    //Touch input ends
+    this.touchend = function(e) {
+        this.states = {
+            'left': false
+            , 'right': false
+            , 'forward': false
+            , 'backward': false
+        };
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    this.codes = {
+        37: 'left'
+        , 39: 'right'
+        , 38: 'forward'
+        , 40: 'backward'
+    };
+    this.states = {
+        'left': false
+        , 'right': false
+        , 'forward': false
+        , 'backward': false
+    };
+    document.addEventListener('keydown', this.keypress.bind(this, true), false);
+    document.addEventListener('keyup', this.keypress.bind(this, false), false);
+    document.addEventListener('touchstart', this.touch.bind(this), false);
+    document.addEventListener('touchmove', this.touch.bind(this), false);
+    document.addEventListener('touchend', this.touchend.bind(this), false);
+
+}
+
+//Player object
+//
+//Contains the properties that have to do with the player character (e.g. movement)
+function Player(x, y, direction) {
+
+    //Walking method for the player
+    this.walk = function(distance, level) {
+        var context = Math.cos(this.direction) * distance;
+        var distancey = Math.sin(this.direction) * distance;
+        //If there's no wall here, move forward till something else happens
+        if (level.get(this.x + context, this.y) <= 0) {
+            this.x += context;
+        }
+        if (level.get(this.x, this.y + distancey) <= 0) {
+            this.y += distancey;
+        }
+        this.momentum == this.momentum + distance;
+    }
+
+    //Rotation method for the player
+    this.rotate = function(angle) {
+        this.direction = (this.direction + angle + FULLCIRCLE) % FULLCIRCLE;
+    }
+
+    //Update the player's position
+    this.update = function(control, level, elapsedtime) {
+        if (control.forward) {
+            this.walk(3 * elapsedtime, level);
+        }
+        if (control.backward) {
+            this.walk(-3 * elapsedtime, level);
+        }
+        if (control.left) {
+            this.rotate(-1 * PI * elapsedtime);
+        }
+        if (control.right) {
+            this.rotate(PI * elapsedtime);
+        }
+    }
+
+    //Other properties
+    this.x = x;
+    this.y = y;
+    this.momentum = 0;
+    this.direction = direction;
+    this.health = 100;
+    //this.weapon = ""; None yet
+}
---
+++ view.js
@@ -0,0 +1,77 @@
+//view.js
+//Contains the player's view
+var USERAGENTS = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+var PI = Math.PI
+var FULLCIRCLE = PI * 2;
+
+function View(canvas, resolution, focallength) {
+
+    this.render = function(player, level) {
+        this.drawsky(player.direction, level.skybox, level.light);
+        this.drawcolumns(player, level);
+    }
+
+    this.drawsky = function(direction, sky, ambient) {
+        var width = sky.width * (this.height / sky.height) * 2;
+        var left = (direction / FULLCIRCLE) * -width;
+
+        this.context.save();
+        this.context.drawImage(sky.image, left, 0, width, this.height);
+        if (left < width - this.width) {
+            this.context.drawImage(sky.image, left + width, 0, width, this.height);
+        }
+        this.context.restore();
+    }
+
+    this.drawonecolumn = function(column, viewbeam, angle, level) {
+        var hit = -1;
+        var context = this.context;
+        var texture = level.walltexture;
+        var left = Math.floor(column * this.spacing);
+        var width = Math.ceil(this.spacing);
+
+        while (++hit < viewbeam.length && viewbeam[hit].height <= 0);
+
+        for (var s = viewbeam.length - 1; s >= 0; s--) {
+            var step = viewbeam[s];
+
+            if (s === hit) {
+                var textureRemainder = Math.floor(texture.width * step.offset);
+                var wall = this.project(step.height, angle, step.distance);
+                context.globalalpha = 1;
+                context.drawImage(texture.image, textureRemainder, 0, 1, texture.height, left, wall.top, width, wall.height);
+            }
+        }
+    }
+
+    this.drawcolumns = function(player, level) {
+        this.context.save();
+        for (var column = 0; column < this.resolution; column++) {
+            var x = column / this.resolution - 0.5;
+            var angle = Math.atan2(x, this.focallength);
+            var viewbeam = level.cast(player, player.direction + angle, this.range);
+            this.drawonecolumn(column, viewbeam, angle, level);
+        }
+        this.context.restore();
+    }
+
+    this.project = function(height, angle, distance) {
+        var z = distance * Math.cos(angle);
+        var wallheight = this.height * 1.4 * height / z;
+        var floor = this.height / 2 * (1 + 1 / z);
+        return {
+            top: floor - wallheight
+            , height: wallheight
+        };
+    }
+
+    this.context = canvas.getContext('2d');
+    this.width = canvas.width = window.innerWidth * 0.5;
+    this.height = canvas.height = window.innerHeight * 0.5;
+    this.resolution = resolution;
+    this.spacing = this.width / resolution;
+    this.focallength = focallength || 0.8;
+    this.range = USERAGENTS ? 8 : 14;
+    this.lightRange = 5;
+    this.scale = (this.width + this.height) / 1200;
+}
